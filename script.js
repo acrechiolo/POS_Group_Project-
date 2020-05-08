9cbd72d86428d6b3133a8fde61722ee63b74abb6
Alyssa Crechiolo <alyssacrechiolo@gmail.com>
2020-05-08 10:57:01 -0400

    still working

--- script.js
+++ script.js
@@ -11,14 +11,19 @@
 // })
 
 let mainItems = document.querySelector("main");
-let cartTotal = document.querySelector(".cart-total");
+let subTotal = document.querySelector(".sub-total");
 let salesTax = document.querySelector(".sales-tax");
+let finalTotal = document.querySelector(".final-total");
 let currentTotal = 0;
+let tax = 0.06;
 
 mainItems.addEventListener("click", (event) => {
     if (event.target.classList.contains("button")) {
         let amount = Number(event.target.getAttribute("data-amount"))
-        currentTotal += amount;
-        cartTotal.innerText = `Current Total: $${currentTotal.toFixed(2)}`
+        let sub = currentTotal += amount;
+        subTotal.innerText = `Sub-Total: $${sub.toFixed(2)}`;
+        let taxTotal = sub * tax;
+        salesTax.innerText = `Sales Tax: $${taxTotal.toFixed(2)}`;
+
     }
 })
