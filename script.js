7a9878c366a1829c7ff2aac4af8210db66a6d3c0
Alyssa Crechiolo <alyssacrechiolo@gmail.com>
2020-05-08 11:22:00 -0400

    fixed html error

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// let vendingMachine = document.querySelector(".vending-machine");
-// let total = document.querySelector(".amount-due");
-// let currentTotal = 0;
-
-// vendingMachine.addEventListener("click", (event) => {
-//     if (event.target.classList.contains("button")) {
-//         let amount = Number(event.target.getAttribute("data-amount"))
-//         currentTotal += amount;
-//         total.innerText = `Total: $${currentTotal.toFixed(2)}`
-//     }
-// })
-
+//CART ADD UP
 let mainItems = document.querySelector("main");
 let subTotal = document.querySelector(".sub-total");
 let salesTax = document.querySelector(".sales-tax");
@@ -19,11 +8,14 @@ let tax = 0.06;
 
 mainItems.addEventListener("click", (event) => {
     if (event.target.classList.contains("button")) {
-        let amount = Number(event.target.getAttribute("data-amount"))
+        let amount = Number(event.target.getAttribute("data-amount"));
         let sub = currentTotal += amount;
         subTotal.innerText = `Sub-Total: $${sub.toFixed(2)}`;
         let taxTotal = sub * tax;
         salesTax.innerText = `Sales Tax: $${taxTotal.toFixed(2)}`;
-
+        let tTotal = taxTotal + sub;
+        finalTotal.innerText = `Current Total: $${tTotal.toFixed(2)}`;
     }
 })
+
+//SLOOOOP
